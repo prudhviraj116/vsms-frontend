ea1d1dac4eb513fba8ebf6e2c436adc08f82db67
prudhviraj116 <prudhvirajsuthapalli@gmail.com>
2025-03-07 15:12:16 +0530

    Add files via upload

---
+++ src/Students/StudentInfoForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StudentInfoForm = () => {
+  return (
+    <div>
+      <h2>Student Info Form</h2>
+      <p>This is a placeholder for the student information form.</p>
+    </div>
+  );
+};
+
+export default StudentInfoForm;
+
